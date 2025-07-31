774c4e4663f15dba5926d0354af0c345a281a868
RamsesCode <RamsesSanchez25@gmail.com>
2025-07-31 13:49:30 -0400

    disregard the new file it was comment out entirely but it can be useful in the past again once we want to implement the get routes for the song names and title

---
+++ api/spotify.js
@@ -0,0 +1,57 @@
+// const express = require("express");
+// const axios = require("axios");
+// const { authenticateJWT } = require("./Auth"); 
+
+// const router = express.Router();
+
+// router.get("/current-track", authenticateJWT, async (req, res) => {
+//   const accessToken = req.user.spotifyAccessToken; 
+
+//   if (!accessToken) {
+//     return res.status(401).json({ error: "Spotify access token missing" });
+//   }
+
+//   try {
+//     const current = await axios.get("https://api.spotify.com/v1/me/player/currently-playing", {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     });
+
+//     if (current.status === 200 && current.data && current.data.item) {
+//       const item = current.data.item;
+//       return res.json({
+//         title: item.name,
+//         artist: item.artists.map((a) => a.name).join(", "),
+//         albumArt: item.album.images[0]?.url,
+//         preview_url: item.preview_url,
+//       });
+//     }
+//   } catch (err) {
+//     if (err.response?.status !== 204) {
+//       console.error("Error getting current track:", err.response?.data || err.message);
+//     }
+//   }
+
+//   // fallback to recently played
+//   try {
+//     const recent = await axios.get("https://api.spotify.com/v1/me/player/recently-played?limit=1", {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     });
+
+//     const item = recent.data.items[0].track;
+//     return res.json({
+//       title: item.name,
+//       artist: item.artists.map((a) => a.name).join(", "),
+//       albumArt: item.album.images[0]?.url,
+//       preview_url: item.preview_url,
+//     });
+//   } catch (err) {
+//     console.error("Error getting recent track:", err.response?.data || err.message);
+//     return res.status(500).json({ error: "Failed to retrieve track info" });
+//   }
+// });
+
+// module.exports = router;
