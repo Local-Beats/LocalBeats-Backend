254f3f1395d84f96fa9be6f7ff0725bcbbd007f3
MrRobot458 <erivas157@gmail.com>
2025-08-04 15:35:40 -0400

    Updated PlaylistSong and ListeningSession db models

--- database/listeningSession.js
+++ database/listeningSession.js
@@ -2,25 +2,31 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 const ListeningSession = db.define("listening_session", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   status: {
     type: DataTypes.ENUM("playing", "paused", "stopped"),
     allowNull: false,
     defaultValue: "stopped",
   },
   user_id: {
-    type: DataTypes.INT,
+    type: DataTypes.INTEGER,
     allowNull: false,
   },
   song_id: {
-    type: DataTypes.INT,
+    type: DataTypes.INTEGER,
     allowNull: false,
   },
-  started_at: {
-    type: DataTypes.TIMESTAMP,
-  },
   ended_at: {
-    type: DataTypes.TIMESTAMP,
+    type: DataTypes.DATE,
+    allowNull: true,
   },
+}, {
+  timestamps: true,  // enable Sequelize to auto-manage createdAt/updatedAt
+  underscored: true, // auto adds created_at and updated_at correctly
 });
 
 module.exports = ListeningSession;
--- database/playlistSong.js
+++ database/playlistSong.js
@@ -2,14 +2,22 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 const PlaylistSong = db.define("playlist_song", {
-  playlist_id: {
-    type: DataTypes.INT,
-    allowNull: false,
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
   song_id: {
-    type: DataTypes.INT,
+    type: DataTypes.INTEGER,
     allowNull: false,
   },
+  playlist_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,  // enable Sequelize to auto-manage createdAt/updatedAt
+  underscored: true, // auto adds created_at and updated_at correctly
 });
 
 module.exports = PlaylistSong;
