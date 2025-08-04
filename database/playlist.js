e843c7e214c465e95471db65640af2ba24a190a0
MrRobot458 <erivas157@gmail.com>
2025-08-04 12:06:59 -0400

    Updated Playlist db model

--- database/playlist.js
+++ database/playlist.js
@@ -2,19 +2,27 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 const Playlist = db.define("playlist", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   name: {
     type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "Untitled",
   },
   user_id: {
-    type: DataTypes.INT,
+    type: DataTypes.INTEGER,
     allowNull: false,
   },
-
   created_at: {
     type: DataTypes.TIMESTAMP,
+    allowNull: true,
   },
   updated_at: {
     type: DataTypes.TIMESTAMP,
+    allowNull: true,
   },
 });
 
