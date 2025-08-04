5f2b2c591b7f2872b3db41a5754d59eb2bbcda63
rend1027 <rend1027@yahoo.com>
2025-08-04 00:06:18 -0400

    added missing jwt import

--- auth/spotify.js
+++ auth/spotify.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authenticateJWT } = require("./middleware");
+const jwt = require("jsonwebtoken");
 const axios = require('axios');
 const { User } = require("../database");
 
