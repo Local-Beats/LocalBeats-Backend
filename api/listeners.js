df6c1f4afd3bc3939c5b58e716ccd3b255edad40
Florencio Rendon <rend1027@yahoo.com>
2025-08-05 15:48:49 -0400

    created endpoint to create listening session

--- api/listeners.js
+++ api/listeners.js
@@ -4,15 +4,18 @@ const { authenticateJWT } = require("../auth/middleware");
 const { User } = require("../database")
 const { ListeningSession } = require("../database");
 
-router.get("/sync", authenticateJWT, async (req, res) => {
+router.get("/listeners", authenticateJWT, async (req, res) => {
     const listeningSession = await ListeningSession.findAll(
         {
             where: { status: "playing" || "paused" },
             include: { User },
             attributes: ["spotify_display_name", "spotify_image"],
         })
+})
 
-
+router.post("/listeners", authenticateJWT, async (req, res) => {
+    const trackData = req.data
+    const listeningSession = await ListeningSession.create(trackData)
 })
 
 module.exports = router;
