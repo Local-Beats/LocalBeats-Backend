64383f63cb32c87ef4902973e2c0501d254e2b5c
Florencio Rendon <rend1027@yahoo.com>
2025-08-05 15:18:08 -0400

    fixed- route path mismatch

--- api/index.js
+++ api/index.js
@@ -7,6 +7,6 @@ const listenerRouter = require("./listeners");
 
 router.use("/test-db", testDbRouter);
 router.use("/spotify", spotifyRouter);
-router.use("/listener", listenerRouter),
+router.use("/listeners", listenerRouter),
 
     module.exports = router;
